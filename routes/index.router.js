5416076b4b659533d5235d54644382520093bb32
Дина Черных <dinachernykh1@gmail.com>
2021-11-18 12:20:34 +0300

    categories numero uno

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
+const { Category } = require('../db/models');
 
 router.route('/')
   .get(async (req, res) => {
-    res.render('index');
+    const categories = await Category.findAll({ raw: true });
+    res.render('index', { categories });
   });
 
 module.exports = router;
